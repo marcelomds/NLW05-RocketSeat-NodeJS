637eda04d5a9df59c842af2fe836043491113e24
Marcelo Moreira <marcelo_mmds@hotmail.com.br>
2021-04-24 10:07:43 -0300

    finalizado Aula 03 - Criado todas as Tabelas

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,28 @@
 import { Router } from 'express';
-import {SettingsController} from "./controllers/SettingsController";
+import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
+import { MessagesController } from "./controllers/MessagesController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
+/**
+ * Rotas de Settings
+ */
 routes.post('/settings', settingsController.create);
+
+/**
+ * Rotas de Usuários
+ */
+routes.post('/users', usersController.create);
+
+/**
+ * Rotas de Mensagens - CHAT
+ */
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes };
---
+++ src/services/UserService.ts
@@ -0,0 +1,37 @@
+import {getCustomRepository, Repository} from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import {User} from "../entities/User";
+
+
+class UserService {
+
+    private usersRepository: Repository<User>
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {
+
+        // Verificar se Usuário Existe
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        // Se existir, retorna User
+        if (userExists) {
+            return userExists;
+        }
+
+        // Se NÃO existir, Salva no Banco de Dados
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { UserService };
