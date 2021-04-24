8cd80e8dd014f3a526031e7bddcbc02e5e62c8a4
Marcelo Moreira <marcelo_mmds@hotmail.com.br>
2021-04-23 22:10:49 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import './database/index';
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => console.log("Server is running on port: 3333"));
